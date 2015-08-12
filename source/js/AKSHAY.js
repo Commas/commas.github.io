0eec888ff6ca377adfd50f4d2eda0db13fb8022e
BillyZhong <billyczhong@gmail.com>
2015-08-12 15:53:05 -0700

    Akshay Randomizing Algorithm Optimized

--- source/js/AKSHAY.js
+++ source/js/AKSHAY.js
@@ -8,7 +8,5 @@ function AKSHAY(){
 		r = Math.floor((Math.random() * 28) + 1);
 		$(this).attr('src', 'source/img/akshay/akshay' + ("0" + r).slice(-2) + '.JPG');
 	});
-	var akshayface = ['source/img/akshay/akshay02.JPG','source/img/akshay/akshay04.JPG','source/img/akshay/akshay05.JPG','source/img/akshay/akshay06.JPG','source/img/akshay/akshay08.JPG','source/img/akshay/akshay09.JPG','source/img/akshay/akshay10.JPG','source/img/akshay/akshay11.JPG','source/img/akshay/akshay12.JPG','source/img/akshay/akshay13.JPG','source/img/akshay/akshay14.JPG','source/img/akshay/akshay15.JPG','source/img/akshay/akshay16.JPG','source/img/akshay/akshay17.JPG','source/img/akshay/akshay18.JPG','source/img/akshay/akshay19.JPG','source/img/akshay/akshay21.JPG','source/img/akshay/akshay22.JPG','source/img/akshay/akshay24.JPG','source/img/akshay/akshay25.JPG'];
-	r = Math.floor(Math.random() * akshayface.length);
-	$('#favicon').attr("href",akshayface[r]);
+	$('#favicon').attr("href","source/img/akshay/akshay11.JPG");
 }
