98a333a0aa9b3c27f9916ff931a6083992748c1f
BillyZhong <billyczhong@gmail.com>
2015-08-12 15:02:48 -0700

    Kush's face

--- source/js/AKSHAY.js
+++ source/js/AKSHAY.js
@@ -1,4 +1,9 @@
 function AKSHAY(){
 	$('.responsive-headline').text("AKSHAY!");
-	$('#desc').text("I am the apotheosized FIFTH COMMA")
+	$('#desc').text("I am the apotheosized FIFTH COMMA");
+	var img = $('img');
+	for(var i = 0; i < img.length; i++){
+		var r = Math.floor((Math.random() * 28) + 1);
+		img[i].attr('src', 'source/img/akshay/akshay' + ("0" + r).slice(-2) + '.JPG')
+	}
 }
