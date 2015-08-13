0f0b4c8bd7ce1734ee2649a22b7ff82ecadc9792
BillyZhong <billyczhong@gmail.com>
2015-08-12 19:01:28 -0700

    Ty kim

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
 	$('#loader').hide();
-	$('#loader').css({'background-color':'#0F0F0F'});
+	$('body').css({'background-color':'#0F0F0F'});
 	$('#container').css({'overflow':'visible'});
 	$('#container').css('opacity', 1);
 });
