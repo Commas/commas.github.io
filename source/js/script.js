2b768c28378452c0f1a76d141f7feb66b08546ad
BillyZhong <billyczhong@gmail.com>
2015-08-12 17:44:18 -0700

    Loader

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-	$('<img/>').attr('src', 'source/img/loader.gif').load(function() {
-   		$(this).remove();
-		$('.modal').css('background-image', 'url(source/img/loader.gif)');
-		$('body').attr('class','loading');
-		setTimeout(function(){
-			$(window).load(function(){
-				$('body').attr('class','');
-			});
-		}, 3000);
-	});
+$(window).load(function(){
+	$('body').attr('class','');
 });
