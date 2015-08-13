6ae26c96abfaa9a2fc51b068f90f2c524cf9dfc5
BillyZhong <billyczhong@gmail.com>
2015-08-12 17:36:52 -0700

    Loader

--- source/js/script.js
+++ source/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		$('body').attr('class','loading');
 	});
 });
-//$(window).load(function(){
-//	$('body').attr('class','');
-//});
+
+$(window).load(function(){
+	$('body').attr('class','');
+});
